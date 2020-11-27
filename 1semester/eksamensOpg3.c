a17c611be71f9ba812bd1c4e8041cd8ceec02798
GrimmFantasy <casper11andersen@gmail.com>
2020-11-27 10:53:41 +0100

    eksOPG3 missing score counting and leaderbord

--- 1semester/eksamensOpg3.c
+++ 1semester/eksamensOpg3.c
@@ -2,26 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 typedef struct kamp{
-    char ugedag[3];
-    char dato[4];
-    char kl[4];
-    char hold1[3];
-    char hold2[3];
+    char ugedag[4];
+    char dato[5];
+    char kl[5];
+    char hold1[4];
+    char hold2[4];
     int score1;
     int score2;
     int tilskuer;
 }kamp;
 
 typedef struct hold{
-    char holdNavn[3];
+    char holdNavn[4];
     int point;
     int scoretMaal;
     int modstannerMaal;
 }hold;
 
 void read_file(kamp(*), FILE(*));
+void create_team(hold(*), kamp(*));
+void filter_team(hold(*), char(*), int(*));
+
 int main(){
-    kamp kampArr[128] /*= malloc(128*sizeof(struct kamp))*/;
+    kamp kampArr[128];
     hold holdArr[14];
     FILE *fp = fopen("kampe-2019-2020.txt", "r");
     
@@ -32,11 +35,14 @@ int main(){
 
     printf("Reading file and gatering data.\n");
     read_file(kampArr, fp);
-    
+    printf("Creating struct for each team\n");
+    create_team(holdArr, kampArr);
+    printf("Starting score keeping\n\n");
+
     return 0;
 }
 
-/**/
+/*Vi læser filen og opsamler det relevente infomation*/
 void read_file(kamp *kampArr, FILE *fp){
     char temp[10];
     int i;
@@ -51,20 +57,24 @@ void read_file(kamp *kampArr, FILE *fp){
             switch (i)
             {
                 case 0:
-                    
-                    strcpy(kampArr[j].ugedag, temp);    
+                    strcpy(kampArr[j].ugedag, temp);
+                    kampArr[j].ugedag[4] = '\0';
                     break;
                 case 1:
                     strcpy(kampArr[j].dato,temp);
+                    kampArr[j].dato[5] = '\0';
                     break;
                 case 2:
                     strcpy(kampArr[j].kl, temp);
+                    kampArr[j].kl[5] = '\0';
                     break;
                 case 3:
                     strcpy(kampArr[j].hold1, temp);
+                    kampArr[j].hold1[4] = '\0';
                     break;
                 case 5:
                     strcpy(kampArr[j].hold2, temp);
+                    kampArr[j].hold2[4] = '\0';
                     break;
                 case 6:
                     kampArr[j].score1 = atoi(temp);
@@ -80,4 +90,17 @@ void read_file(kamp *kampArr, FILE *fp){
             }
         }
     }
+}
+
+/*vi går ud fra at alle hold spiller både på ude bane og hjemme bane*/
+void create_team(hold *holdArr, kamp *kampArr){
+    
+    int i;
+    int j;
+    
+    for(i = 0, j = 7; i < 7; i++, j++){
+        strcpy(holdArr[i].holdNavn, kampArr[i].hold1);
+        strcpy(holdArr[j].holdNavn, kampArr[i].hold2);
+    }
+    
 }
