29743bc641a4956f0635a26c433bed7deaf1a3fe
GrimmFantasy <casper11andersen@gmail.com>
2020-11-30 19:32:26 +0100

    eks 3 done, vary nice

--- 1semester/eksamensOpg3.c
+++ 1semester/eksamensOpg3.c
@@ -24,16 +24,28 @@ void create_team(hold(*), kamp(*));
 void filter_team(hold(*), char(*), int(*));
 void score_count(hold(*), kamp(*));
 void search_for_team(int, hold(*), kamp(*));
+void leard_bord(hold(*));
 
 int main(){
     kamp kampArr[128];
     hold holdArr[14];
     FILE *fp = fopen("kampe-2019-2020.txt", "r");
-    
     if (fp == NULL){
         printf("Could not read from file\n");
         return EXIT_FAILURE;
     }
+
+    int i;
+    int j = 0;
+
+    for (i = 0; i < 14; i++)
+    {
+        holdArr[i].point = 0;
+        holdArr[i].scoretMaal = 0;
+        holdArr[i].modstannerMaal = 0;
+    }
+    
+    holdArr[i].point = 0;
 
     printf("Reading file and gatering data.\n");
     read_file(kampArr, fp);
@@ -41,6 +53,16 @@ int main(){
     create_team(holdArr, kampArr);
     printf("Starting score keeping\n\n");
     score_count(holdArr, kampArr);
+    printf("Sorting teams by score\n");
+    leard_bord(holdArr);
+
+    /*her looper vi igennem fra bunden op, da vi har sotered det på den måde*/
+    printf("Plads  Holdnavn Point Mål-af-hold Mål-mod-hold\n");
+    for (i = 13; i > -1; i--)
+    {
+        j++;
+        printf(" %d:      %s     %d       %d          %d\n", j, holdArr[i].holdNavn, holdArr[i].point, holdArr[i].scoretMaal, holdArr[i].modstannerMaal);
+    }
     return 0;
 }
 
@@ -109,13 +131,14 @@ void create_team(hold *holdArr, kamp *kampArr){
     
 }
 
+/*her køre man i gennem alle kampen og optælle point i gennem seachs for team*/
 void score_count(hold *holdArr, kamp *kampArr){
     
     int i;
     int j;
     
     for(i = 0; i < 128; i++){
-        printf("\n       Dato     kl        Hold       Score    Tilskuer\n");
+        /*printf("\n       Date     kl        Team       Score    Tilskuer\n");
         printf("%s   ", kampArr[i].ugedag);
         printf("%s   ", kampArr[i].dato);
         printf("%s   ", kampArr[i].kl);
@@ -123,45 +146,71 @@ void score_count(hold *holdArr, kamp *kampArr){
         printf("%s   ", kampArr[i].hold2);
         printf("%d  -  ", kampArr[i].score1);
         printf("%d   ", kampArr[i].score2);
-        printf("%d\n", kampArr[i].tilskuer);
+        printf("%d\n", kampArr[i].tilskuer);*/
         search_for_team(i, holdArr, kampArr);
     }
 }
 
+/*matcher holdene med deres score og tæller score*/
 void search_for_team(int pos, hold *holdArr, kamp *kampArr){
     
     int i;
-
+    
     for ( i = 0; i < 14; i++)
     {
-        if (kampArr[pos].hold1 == holdArr[i].holdNavn){
-            holdArr[i].scoretMaal = kampArr[pos].score1;
-            holdArr[i].modstannerMaal = kampArr[pos].score2;
+        
+        if (strcmp(kampArr[pos].hold1, holdArr[i].holdNavn) == 0){
+            
+            holdArr[i].scoretMaal = holdArr[i].scoretMaal + kampArr[pos].score1;
+            holdArr[i].modstannerMaal = holdArr[i].modstannerMaal + kampArr[pos].score2;
 
             if(kampArr[pos].score1 > kampArr[pos].score2){
-                holdArr[i].point = 3;
+                holdArr[i].point = holdArr[i].point + 3;
             }
             if(kampArr[pos].score1 == kampArr[pos].score2){
-                holdArr[i].point = 1;
+                holdArr[i].point = holdArr[i].point + 1;
             }
-            break;
+            else{
+                break;
+            }
+            
         }
     }
 
     for ( i = 0; i < 14; i++)
     {
-        if (kampArr[pos].hold2 == holdArr[i].holdNavn){
-            holdArr[i].scoretMaal = kampArr[pos].score2;
-            holdArr[i].modstannerMaal = kampArr[pos].score1;
+        if (strcmp(kampArr[pos].hold2, holdArr[i].holdNavn) == 0){
+            holdArr[i].scoretMaal = holdArr[i].scoretMaal + kampArr[pos].score2;
+            holdArr[i].modstannerMaal = holdArr[i].modstannerMaal + kampArr[pos].score1;
 
             if(kampArr[pos].score2 > kampArr[pos].score1){
-                holdArr[i].point = 3;
+                holdArr[i].point = holdArr[i].point + 3;
             }
             if(kampArr[pos].score2 == kampArr[pos].score1){
-                holdArr[i].point = 1;
+                holdArr[i].point = holdArr[i].point + 1;
             }
             break;
         }
     }
-    
+}
+
+/*Bruger en simple sotering algorime til at plasere det højste nederest og det lavest*
+øvers*/
+void leard_bord(hold *holdArr){
+    int i, j;
+    hold temp;
+    for (j = 0; j < 14; j++)
+    {
+        for (i = 0; i < 14; i++)
+        {
+            if(holdArr[i].point > holdArr[i+1].point && i + 1 != 14){
+                
+                temp = holdArr[i];
+                holdArr[i] = holdArr[i+1];
+                holdArr[i+1] = temp;
+            }
+            
+        }
+    }
+        
 }
